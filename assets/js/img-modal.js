e515a0bc5bca40805ad5d31da787cb4e04b713c2
Oscar Tinajero <tinajero117.oscar@gmail.com>
2022-11-13 23:03:41 -0600

    Add Images Modal CSS & JS

---
+++ assets/js/img-modal.js
@@ -0,0 +1,27 @@
+const imgMap = {
+    "html-intro" : 'como-crear-una-pagina-web.jpg',
+    "mental-maps" : 'mapas-mentales-desde-cero.jpg',
+    "meli-intro" : 'mercadolibre-desde-cero-a-experto.jpg',
+    "learn-it" : 'aprendetelo.jpg',
+    "leadership" : 'liderazgo-101.jpg'
+};
+
+let modal = document.getElementById('myModal');
+
+let imgModal = document.getElementById('img');
+
+let allItems = document.querySelectorAll('.portfolio-item');
+
+allItems.forEach(item => {
+    item.addEventListener('click', function() {
+        let id = this.getAttribute("id");
+        let imgName = imgMap[id];
+        let path = './assets/img/Docs/'+imgName;
+        modal.style.display='block';
+        imgModal.src = path;
+    })
+});
+
+let imgClose = document.getElementsByClassName('img-close')[0];
+
+imgClose.onclick = ()=> modal.style.display = 'none';
